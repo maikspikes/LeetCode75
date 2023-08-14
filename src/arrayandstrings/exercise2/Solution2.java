8b11d2fe0883f8016164cd1efa8c63e2d4b76687
Maik Verheij <maik.verheij@digitalsurvivalcompany.com>
2023-08-14 13:35:47 +0200

    add 2nd possible solution for exercise 2

--- src/arrayandstrings/exercise2/Solution2.java
+++ src/arrayandstrings/exercise2/Solution2.java
@@ -1,2 +1,31 @@
-package arrayandstrings.exercise2;public class Solution2 {
+package arrayandstrings.exercise2;
+
+public class Solution2 {
+    public String gcdOfStrings(String str1, String str2) {
+
+        // Find the greatest common divisor (GCD) of the lengths of str1 and str2
+        int len1 = str1.length();
+        int len2 = str2.length();
+        int gcd = gcd(len1, len2);
+
+        // Check if str2 is a repeated substring of str1, and vice versa
+        String repeatedStr = str1.substring(0, gcd);
+        if (str1.equals(repeatedStr.repeat(len1 / gcd))
+                && str2.equals(repeatedStr.repeat(len2 / gcd))) {
+            return repeatedStr;
+        }
+
+        // If no common divisor, return empty string
+        return "";
+    }
+
+    // Helper function to compute the greatest common divisor (gcd) of two numbers
+    public static int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+        return gcd(b, a % b);
+    }
+
 }
+
