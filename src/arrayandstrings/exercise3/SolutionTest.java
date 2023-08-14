9690c59784b253c3e079324e2491511e0e8801df
Maik Verheij <maik.verheij@digitalsurvivalcompany.com>
2023-08-14 14:07:13 +0200

    add testcase for exercise 3

--- src/arrayandstrings/exercise3/SolutionTest.java
+++ src/arrayandstrings/exercise3/SolutionTest.java
@@ -1,2 +1,22 @@
-package arrayandstrings.exercise3;public class SolutionTest {
+package arrayandstrings.exercise3;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionTest {
+
+    @Test
+    void kidsWithCandies() {
+        Solution solution = new Solution();
+
+        //test1
+        int[] candies = {2, 3, 5, 1, 3};
+        int extraCandies = 3;
+        List<Boolean> result1 = solution.kidsWithCandies(candies, extraCandies);
+        assertArrayEquals(new Boolean[]{true, true, true, false, true}, result1.toArray());
+
+    }
 }
