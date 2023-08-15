e36e017bb72df1b4daf274e30ffcbc7cf2be36db
Maik Verheij <maik.verheij@digitalsurvivalcompany.com>
2023-08-15 10:28:43 +0200

    add the read.me file, and create a solution for exercise number 4

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # LeetCode75
-# LeetCode75
+
+This project is to keep track of my progress during this study plan, and to keep the practice of using git alive.
+I will be trying out multiple solutions for the same problem.
---
+++ src/exercise4/Solution.java
@@ -0,0 +1,22 @@
+package exercise4;
+
+class Solution {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+
+        int count = 0;
+        for (int i = 0; i < flowerbed.length; i++) {
+            if (flowerbed[i] == 0) {
+                // Check if the left and right plots are empty.
+                boolean emptyLeftPlot = (i == 0) || (flowerbed[i - 1] == 0);
+                boolean emptyRightPlot = (i == flowerbed.length - 1) || (flowerbed[i + 1] == 0);
+
+                if (emptyLeftPlot && emptyRightPlot) {
+                    flowerbed[i] = 1;
+                    count++;
+                }
+            }
+        }
+
+        return count >= n;
+    }
+}
