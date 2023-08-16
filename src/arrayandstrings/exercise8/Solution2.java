a6576fb4a0a49ce772303dc778a54c92c009c128
Maik Verheij <maik.verheij@digitalsurvivalcompany.com>
2023-08-16 17:11:25 +0200

    Add 2nd solution with the use of the greedy algorithm

--- src/arrayandstrings/exercise8/Solution2.java
+++ src/arrayandstrings/exercise8/Solution2.java
@@ -1,4 +1,22 @@
 package arrayandstrings.exercise8;
 
 public class Solution2 {
+    //this is an example of the greedy algorithm
+    public boolean increasingTriplet(int[] nums) {
+
+        int firstmin = Integer.MAX_VALUE;
+        int secondmin = Integer.MAX_VALUE;
+
+        for (int num : nums) {
+            if (num <= firstmin){
+                firstmin = num;
+            } else if (num <= secondmin) {
+                secondmin = num;
+            } else {
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
