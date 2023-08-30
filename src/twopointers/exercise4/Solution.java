18ca13096d74add4a72ebe9763e8747d24f98aca
Maik Verheij <maik.verheij@digitalsurvivalcompany.com>
2023-08-30 10:34:13 +0200

    solve exercise 4

--- src/twopointers/exercise4/Solution.java
+++ src/twopointers/exercise4/Solution.java
@@ -1,2 +1,29 @@
-package twopointers.exercise4;public class Solution {
+package twopointers.exercise4;
+
+import java.util.Arrays;
+
+class Solution {
+    public static int maxOperations(int[] nums, int k) {
+
+        Arrays.sort(nums);
+        int left = 0;
+        int right = nums.length - 1;
+        int count = 0;
+
+        while (left < right) {
+            int sum = nums[left] + nums[right];
+
+            if (sum == k) {
+                count++;
+                left++;
+                right--;
+            } else if (sum < k) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return count;
+    }
 }
