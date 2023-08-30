5001f109bb211de03067d9477faffeea8aef0de4
Maik Verheij <maik.verheij@digitalsurvivalcompany.com>
2023-08-30 11:15:30 +0200

    add comments to further explain code

--- src/twopointers/exercise4/Solution.java
+++ src/twopointers/exercise4/Solution.java
@@ -5,6 +5,12 @@ import java.util.Arrays;
 class Solution {
     public static int maxOperations(int[] nums, int k) {
 
+        /*
+        If the array is sorted in ascending order, it allows you to take advantage of the ordering to efficiently
+        narrow down your search for pairs. This is because if you know the current sum is smaller than k,
+        you can increase the left pointer to consider larger values, and if the current sum is greater than k,
+        you can decrease the right pointer to consider smaller values.
+         */
         Arrays.sort(nums);
         int left = 0;
         int right = nums.length - 1;
@@ -13,6 +19,8 @@ class Solution {
         while (left < right) {
             int sum = nums[left] + nums[right];
 
+            //the array should shrink since the far right and far left numbers, give the result of k.
+            // They can now be removed
             if (sum == k) {
                 count++;
                 left++;
